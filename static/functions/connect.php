348c0a9c963e414dba0a32a848a79559574a90e9
PondJaᵀᴴ <palapon2545@gmail.com>
2021-01-28 14:43:35 +0700

    *

--- static/functions/connect.php
+++ static/functions/connect.php
@@ -1,21 +1,12 @@
 <?php
     ob_start();
     session_start();
-<<<<<<< Updated upstream
     $start_time = microtime(TRUE);
-
-    $dbhost = "pondhub.ga";
-    $dbuser = "pondjaco";
-    $dbpass = "11032545";
-    $dbdatabase = "pondjaco_srinagarindhospital";
-    $dbdatabaseForum = "pondjaco_srinagarindhospitalforum";
-=======
     $dbhost = "196.53.250.111";
     $dbuser = "pharmmd";
     $dbpass = "22jbY$?*ZKXz^-a9X4ucrEgD";
     $dbdatabase = "pharmmd_pharmmd";
     $dbdatabaseForum = "pharmmd_pharmmd_forum";
->>>>>>> Stashed changes
     $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbdatabase); 
     $connForum = mysqli_connect($dbhost,$dbuser,$dbpass,$dbdatabaseForum); 
     
